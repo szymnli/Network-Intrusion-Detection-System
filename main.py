d6a207708f45cfbf66b18707899e87e69d438f75
Szymon Linda <szymonlinda@protonmail.com>
2026-03-06 22:03:32 +0100

    Format sniffing output

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
+from rich import print
+from rich.console import Console
+from rich.table import Table
 from scapy.all import *
+
+IFACE = "wlp0s20f3"
 
 
 def packet_callback(packet):
-    print(packet.show())
+    # Detect:
+    # port scan
+    # SYN flood
+    # ICMP sweep
+    # Suspicious DNS
+
+    if not packet.haslayer(IP):
+        return
+    
+    src = packet[IP].src
+    dst = packet[IP].dst
+
+    if packet.haslayer(TCP):
+        proto = "TCP"
+        sport = packet[TCP].sport
+        dport = packet[TCP].dport
+    elif packet.haslayer(UDP):
+        proto = "UDP"
+        sport = packet[UDP].sport
+        dport = packet[UDP].dport
+    elif packet.haslayer(ICMP):
+        proto = "ICMP"
+        sport = None
+        dport = None
+    else:
+        return
+    
+    print(f"[{proto}] {src}:{sport} -> {dst}:{dport}")
 
 
 def main():
     sniff(
         filter="tcp or udp or icmp",
-        iface="wlp0s20f3",
+        iface=IFACE,
         prn=packet_callback,
     )
 
