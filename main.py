58855a223da0e9a72488b830d68e71477be25eee
Szymon Linda <szymonlinda@protonmail.com>
2026-03-07 00:53:08 +0100

    Save sniffer logs to csv

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from rich import print
 from rich.console import Console
 from rich.table import Table
 from scapy.all import *
+import pandas as pd
 
 IFACE = "wlp0s20f3"
+
+packet_log = []
+df = pd.DataFrame(
+    columns=[
+        "Protocol",
+        "Source IP",
+        "Source Port",
+        "Destination IP",
+        "Destination Port",
+        "Packet length",
+        "TCP Flag",
+        "ICMP Type",
+    ]
+)
 
 
 def packet_callback(packet):
@@ -15,7 +30,7 @@ def packet_callback(packet):
 
     if not packet.haslayer(IP):
         return
-    
+
     src = packet[IP].src
     dst = packet[IP].dst
 
@@ -23,18 +38,45 @@ def packet_callback(packet):
         proto = "TCP"
         sport = packet[TCP].sport
         dport = packet[TCP].dport
+        icmp_type = None
+        flag = packet[TCP].flags
     elif packet.haslayer(UDP):
         proto = "UDP"
         sport = packet[UDP].sport
         dport = packet[UDP].dport
+        icmp_type = None
+        flag = None
+
     elif packet.haslayer(ICMP):
         proto = "ICMP"
         sport = None
         dport = None
+        icmp_type = packet[ICMP].type
+        flag = None
+
     else:
         return
-    
-    print(f"[{proto}] {src}:{sport} -> {dst}:{dport}")
+
+    packet_log.append(
+        {
+            "Protocol": proto,
+            "Source IP": src,
+            "Source Port": sport,
+            "Destination IP": dst,
+            "Destination Port": dport,
+            "Packet Length": len(packet),
+            "TCP Flag": flag,
+            "ICMP Type": icmp_type,
+        }
+    )
+    print(
+        f"[{proto}] {src}:{sport} -> {dst}:{dport} ({len(packet)}) -- {flag} -- {icmp_type}"
+    )
+
+
+def save_log():
+    df = pd.DataFrame(packet_log)
+    df.to_csv("capture.csv", index=False)
 
 
 def main():
@@ -43,6 +85,10 @@ def main():
         iface=IFACE,
         prn=packet_callback,
     )
+    print("\nStopping sniffer...")
+    save_log()
+    print(f"Saved {len(packet_log)} packets to capture.csv")
+
 
 
 if __name__ == "__main__":
