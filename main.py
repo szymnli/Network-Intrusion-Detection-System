35da6fad4639288fd7f625b309560fa32d424612
Szymon Linda <szymonlinda@protonmail.com>
2026-03-07 11:27:28 +0100

    Add basic port scan detection

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ from rich.console import Console
 from rich.table import Table
 from scapy.all import *
 import pandas as pd
+from collections import defaultdict
+import time
 
+# Name of the capturing network interface
 IFACE = "wlp0s20f3"
 
 packet_log = []
-df = pd.DataFrame(
-    columns=[
-        "Protocol",
-        "Source IP",
-        "Source Port",
-        "Destination IP",
-        "Destination Port",
-        "Packet length",
-        "TCP Flag",
-        "ICMP Type",
-    ]
-)
-
+port_scan_tracker = defaultdict(set)
+alerted_ips = set()
 
 def packet_callback(packet):
     # Detect:
@@ -40,6 +32,7 @@ def packet_callback(packet):
         dport = packet[TCP].dport
         icmp_type = None
         flag = packet[TCP].flags
+
     elif packet.haslayer(UDP):
         proto = "UDP"
         sport = packet[UDP].sport
@@ -56,6 +49,9 @@ def packet_callback(packet):
 
     else:
         return
+    
+    if proto == "TCP" and str(flag) == "S":
+        detect_port_scan(src, dport)
 
     packet_log.append(
         {
@@ -73,6 +69,15 @@ def packet_callback(packet):
         f"[{proto}] {src}:{sport} -> {dst}:{dport} ({len(packet)}) -- {flag} -- {icmp_type}"
     )
 
+def detect_port_scan(src, dport):
+    # Same src IP addr
+    # Many different dst ports
+    # Everything in a short time
+    port_scan_tracker[src].add(dport)
+
+    if len(port_scan_tracker[src]) > 15 and src not in alerted_ips:
+        alerted_ips.add(src)
+        print(f"[!] PORT SCAN DETECTED — {src} has hit {len(port_scan_tracker[src])} unique ports")
 
 def save_log():
     df = pd.DataFrame(packet_log)
@@ -85,10 +90,12 @@ def main():
         iface=IFACE,
         prn=packet_callback,
     )
+    if len(alerted_ips) > 0:
+        for ip in alerted_ips:
+            print(f"\n[!] {ip} caused an alert")
     print("\nStopping sniffer...")
     save_log()
     print(f"Saved {len(packet_log)} packets to capture.csv")
-
 
 
 if __name__ == "__main__":
